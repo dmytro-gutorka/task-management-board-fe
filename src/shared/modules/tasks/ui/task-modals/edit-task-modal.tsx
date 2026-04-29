2eadb03b0dce92a6a6276bd9b3662fb5ee04d32c
Dmytro Gutorka <dgutorka@gmail.com>
2026-04-29 18:52:03 +0300

    remove unnesesary state + some minor fixes

--- src/shared/modules/tasks/ui/task-modals/edit-task-modal.tsx
+++ src/shared/modules/tasks/ui/task-modals/edit-task-modal.tsx
@@ -1,17 +1,20 @@
 import { ActionModal } from '@/shared/components/modal/ui/action-modal.tsx';
 import { Separator } from '@/shared/components/shadcn/ui/separator.tsx';
+import { mapTaskTiInitialValues } from '../../helpers/mapTaskTiInitialValues.ts';
 import type { TaskFormValues } from '../../model/task-form/tasks-form.types.ts';
+import type { Task } from '../../model/task/task.types.ts';
 
 import { TaskForm } from '../task-forms/task-form.tsx';
 
 interface EditTaskModalProps {
-    initialValues: TaskFormValues;
+    initialValues: Task;
     isOpen: boolean;
     setOpen: (open: boolean) => void;
     onSubmit: (values: TaskFormValues) => void;
 }
 
 export function EditTaskModal({ initialValues, isOpen, setOpen, onSubmit }: EditTaskModalProps) {
+    const formInitialValues = mapTaskTiInitialValues(initialValues);
     const formId = 'edit-task-form';
 
     return (
@@ -26,7 +29,7 @@ export function EditTaskModal({ initialValues, isOpen, setOpen, onSubmit }: Edit
             >
                 <Separator />
 
-                <TaskForm formId={formId} initialValues={initialValues} onSubmit={onSubmit} />
+                <TaskForm formId={formId} initialValues={formInitialValues} onSubmit={onSubmit} />
             </ActionModal>
         </>
     );
