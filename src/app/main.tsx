cae31aa0f234e927d4f7a73ecd8f7d8248a6dd8f
Dmytro Gutorka <dgutorka@gmail.com>
2026-04-30 16:47:15 +0300

    feature: login modules + protected/public routes

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,9 +5,10 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/app/query-client';
-import { Toaster } from 'sonner';
 import { router } from './routes/routes.tsx';
 import { ThemeProvider } from '@/shared/providers/theme-provider/theme-provider';
+import { Toaster } from 'sonner';
+
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
