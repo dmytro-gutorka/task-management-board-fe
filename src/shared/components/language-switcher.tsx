370d7477c5b617f7942d07ce919e153407809a64
Dmytro Gutorka <dgutorka@gmail.com>
2026-04-30 10:04:51 +0300

    feature: add i18n to project + en/uk lovales

--- src/shared/components/language-switcher.tsx
+++ src/shared/components/language-switcher.tsx
@@ -1,8 +1,6 @@
 import { Languages } from 'lucide-react';
-import { useTranslation } from 'react-i18next';
 import { type AppLanguage, languageOptions } from '../modules/i18n/i18n.constants.ts';
 import { useChangeLanguage } from '../modules/i18n/use-change-language.ts';
-import { IconTooltip } from './icon-tooltip.tsx';
 import { Button } from './shadcn/ui/button.tsx';
 import {
     DropdownMenu,
@@ -14,18 +12,15 @@ import {
 
 export function LanguageSwitcher() {
     const { language, changeLanguage } = useChangeLanguage();
-    const { t } = useTranslation(['common']);
 
     return (
         <DropdownMenu>
-            <IconTooltip content={t('switchLanguage', { ns: 'common' })}>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="outline" size="icon">
-                        <Languages className="h-4 w-4" />
-                        <span className="sr-only">Change language</span>
-                    </Button>
-                </DropdownMenuTrigger>
-            </IconTooltip>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                    <Languages className="h-4 w-4" />
+                    <span className="sr-only">Change language</span>
+                </Button>
+            </DropdownMenuTrigger>
 
             <DropdownMenuContent align="end">
                 <DropdownMenuRadioGroup
