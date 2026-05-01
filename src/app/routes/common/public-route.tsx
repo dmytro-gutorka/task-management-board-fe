f1a8ec243023ed97e3d9c102945bdd8d916058bc
Dmytro Gutorka <dgutorka@gmail.com>
2026-05-01 10:15:26 +0300

    refactor: change public and private routes

--- src/app/routes/common/public-route.tsx
+++ src/app/routes/common/public-route.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { isAuthenticated } from '../../../shared/modules/auth/auth-token.helpers.ts';
+import { ROUTES } from './routes.constants.ts';
 
 export function PublicOnlyRoute() {
     if (isAuthenticated()) {
-        return <Navigate to="/tasks" replace />;
+        return <Navigate to={ROUTES.HOME} replace />;
     }
 
     return <Outlet />;
